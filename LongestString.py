73a64408e062702b89f50ec752e11642371dff00
kweku-xvi <nksarps@gmail.com>
2023-01-31 14:02:33 +0000

    readme

---
+++ LongestString.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the number of elements in the array: "))
+m = list(map(int, input("Elements of array: ").strip().split()))
+print(m)
---
+++ dummy.py
@@ -0,0 +1,7 @@
+def list_sort():
+    n = [1, 5, 6, 6]
+    n.sort()
+    print(n)
+
+
+list_sort()
