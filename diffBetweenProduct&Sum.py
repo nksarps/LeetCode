1a66d90e8fdc16ca70197b5945440ace514936b8
kweku-xvi <nksarps@gmail.com>
2023-05-12 19:57:18 +0000

    leetcode

---
+++ diffBetweenProduct&Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        sum = 0
+        prod = 1
+
+        while n > 0:
+            last_number = n % 10
+            sum += last_number
+            prod *= last_number
+            n //= 10
+        
+        return prod - sum
