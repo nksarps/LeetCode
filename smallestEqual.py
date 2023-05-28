87f3167c1e5a9a23e9747fd41aac50244e4be4f1
kweku-xvi <nksarps@gmail.com>
2023-05-28 11:35:38 +0000

    leetcode

---
+++ smallestEqual.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        smallest_num = len(nums)
+        count = 0
+
+        for i in range(len(nums)):
+            if i % 10 == nums[i]:
+                count += 1
+                if i <= smallest_num:
+                    smallest_num = i
+        if count > 0:
+            return smallest_num
+        else:
+            return -1
