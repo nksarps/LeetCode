8a6b1141afeca2abeec4ae22049c6e777168b36c
kweku-xvi <nksarps@gmail.com>
2023-06-02 00:53:29 +0000

    leetcode

--- dummy.py
+++ dummy.py
@@ -1,1 +1,15 @@
-print(10 ** 3)
+class Solution:
+    def majorityElement(self, nums):
+        nums.sort()
+        max_num = nums.count(nums[0])
+        for i in range(len(nums)):
+            n = nums.count(nums[i])
+            print(n, nums[i])
+            if n >= nums.count(nums[i-1]):
+                num = nums[i]
+        return num
+            
+
+nums =[6,5,5]
+soln = Solution()
+print(soln.majorityElement(nums))
