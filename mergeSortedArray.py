610051c1f1a783685d0030447d1a48a91d542dd8
kweku-xvi <nksarps@gmail.com>
2023-10-02 18:45:54 +0000

    leetcode merge sorted array

---
+++ mergeSortedArray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        while not len(nums1) == m:
+            nums1.pop()
+        while not len(nums2) == n:
+            nums2.pop()
+        for num in nums2:
+            nums1.append(num)
+        nums1.sort()
+        
+# link to question, https://leetcode.com/problems/merge-sorted-array/submissions/
