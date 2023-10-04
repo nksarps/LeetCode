4c68bdbce9801658ddb42283d88086e8369956cc
kweku-xvi <nksarps@gmail.com>
2023-10-04 18:58:24 +0000

    leetcode capitalize title

---
+++ capitalize_title.py
@@ -0,0 +1,23 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        arr_title = title.split()
+        return_arr = []
+        return_sentence = ''
+        n = 0
+        for word in arr_title:
+            if len(word) > 2:
+                word = word.capitalize()
+            else:
+                word = word.lower()
+                pass
+            return_arr.append(word)
+        while n < len(return_arr):
+            if not n == len(return_arr) - 1:
+                return_sentence += return_arr[n]
+                return_sentence += ' '
+            else:
+                return_sentence += return_arr[n]
+            n += 1
+        return return_sentence
+
+# link to question, https://leetcode.com/problems/capitalize-the-title/
