200ef3dd4f14afba2d9cb63e08faa69d3a42d1bc
kweku-xvi <nksarps@gmail.com>
2023-10-14 21:12:41 +0000

    leetcode #674

---
+++ length_of_LCIS.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0 
+        max_length = 1 
+        current_length = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                current_length += 1
+                max_length = max(max_length, current_length)
+            else:
+                current_length = 1
+        return max_length
+
+# link to question, https://leetcode.com/problems/longest-continuous-increasing-subsequence/
