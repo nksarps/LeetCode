72f8c681fce8f1892488b75df7fd1aac8c880e73
kweku-xvi <nksarps@gmail.com>
2023-10-16 19:26:20 +0000

    leetcode #154

---
+++ find_min_2.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
+
+# link to question, https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
