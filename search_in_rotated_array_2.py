28acb71cbdc026568ccea3eb352e029988c326d3
kweku-xvi <nksarps@gmail.com>
2023-10-23 18:54:34 +0000

    leetcode #81

---
+++ search_in_rotated_array_2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if target in nums:
+            return True
+        return False 
+
+# link to question, https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+        
