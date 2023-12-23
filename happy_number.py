fbed6a6810edfd02e4b586ce653072bcf97b00e9
kweku-xvi <nksarps@gmail.com>
2023-12-23 17:29:24 +0000

    #202

---
+++ happy_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit) ** 2 for digit in str(n))
+
+        return n == 1
+
+# link to question, https://leetcode.com/problems/happy-number/
