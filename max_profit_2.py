0db73a8a620613cd5c29c20519a2dd81d13d3dab
kweku-xvi <nksarps@gmail.com>
2023-12-29 15:00:56 +0000

    #122

---
+++ max_profit_2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices or len(prices) < 2:
+            return 0
+
+        total_profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                total_profit += prices[i] - prices[i - 1]
+
+        return total_profit
+
+# link, https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
