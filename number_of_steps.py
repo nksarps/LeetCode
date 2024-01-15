cbcb2399c53333ede653fd406b9367b3d0ff4074
kweku-xvi <nksarps@gmail.com>
2024-01-15 08:24:47 +0000

    #1342

---
+++ number_of_steps.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        count = 0
+        while num != 0:
+            if num % 2 == 0:
+                num /= 2
+                count += 1
+            else:
+                num -= 1
+                count += 1
+        return count
+        
+# link to question, https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
