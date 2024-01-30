28d349aa4ae84362030adf71a07ffabf7afc85c5
kweku-xvi <nksarps@gmail.com>
2024-01-30 15:12:00 +0000

    #1929

---
+++ array_concatenation.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = 0
+        ans = []
+        while n < 2:
+            for num in nums:
+                ans.append(num)
+            n += 1
+        return ans
+
+# link to question, https://leetcode.com/problems/concatenation-of-array/
