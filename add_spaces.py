927699a81527f386d02282ec82ebd0d46fa3b132
kweku-xvi <nksarps@gmail.com>
2024-02-03 02:39:12 +0000

    #2109

--- add_spaces.py
+++ add_spaces.py
@@ -1,6 +1,6 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        s = [i for i in re.split("([A-Z][^A-Z]*)", s) if i]
+        s = [i for i in re.split("([A-Z][^A-Z]*)", s) if i] # spliting string wherever there's a capital letter
         result = ''
         counter = 0
         while counter < len(s):
