d7be8a2faec77179b24e41b7712bbc6363472f1c
kweku-xvi <nksarps@gmail.com>
2024-02-03 02:38:21 +0000

    #2109

---
+++ add_spaces.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        s = [i for i in re.split("([A-Z][^A-Z]*)", s) if i]
+        result = ''
+        counter = 0
+        while counter < len(s):
+            result += s[counter]
+            result += ' '
+            counter += 1
+        return result.strip()
+
+#🚧  link, https://leetcode.com/problems/adding-spaces-to-a-string/
