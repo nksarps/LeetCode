dcb46befea1fd76275fa111f99140e8dfbc74d68
kweku-xvi <nksarps@gmail.com>
2025-01-08 09:03:05 +0000

    leetcode 49

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,13 +1,16 @@
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = defaultdict(list)
+        d = {}
 
         for s in strs:
             k = ''.join(sorted(s))
-            d[k].append(s)
+
+            if not k in d:
+                d[k] = [s]
+            else:
+                d[k].append(s)
 
         return list(d.values())
+        
         
 # link https://leetcode.com/problems/group-anagrams
